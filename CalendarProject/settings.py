397b073edc603d0582c202e63c03ff9a04eeecf9
robin sylvester <61312233+robin-sylvester@users.noreply.github.com>
2022-01-17 02:38:26 +0100

    Update settings.py

--- CalendarProject/settings.py
+++ CalendarProject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gr98lne-d20f65%3xm!u@&cr%u$flqr=%&%yc8h9+gr40g$o+9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
